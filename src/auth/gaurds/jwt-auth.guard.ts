d377f0fce709ef31e2449c2b3d4ce38550871a13
MinhLam577 <duynghia0705@gmail.com>
2026-04-22 00:42:07 +0700

    recode login by google

--- src/auth/gaurds/jwt-auth.guard.ts
+++ src/auth/gaurds/jwt-auth.guard.ts
@@ -13,32 +13,32 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     super();
   }
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+  canActivate(context: ExecutionContext) {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
+
     if (isPublic) {
       return true;
     }
-    const canActivate = await super.canActivate(context);
-    if (!canActivate) {
-      return false;
-    }
-    return true;
+
+    return super.canActivate(context);
   }
 
-  //Phương thức handleRequest trong JwtAuthGuard sẽ xử lý kết quả xác thực từ Passport
-  handleRequest(err, user, info, context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    const requestMethod = request.method;
-    const requestPath = request.route?.path;
-    const permissions = user.permissions ?? [];
-    // check permissions : bên validate sẽ trả về user có role - permission
-    if (err || !user) {
-      throw err || new UnauthorizedException('Invalid access token');
-    }
-    return user;
-  }
+  // getRequest(context: ExecutionContext) {
+  //   const ctx = context.switchToHttp();
+  //   const request = ctx.getRequest();
+
+  //   return request.raw ?? request;
+  // }
+
+  // handleRequest(err, user, info, context: ExecutionContext) {
+  //   if (err || !user) {
+  //     throw err || new UnauthorizedException('Invalid access token');
+  //   }
+  //   const req = context.switchToHttp().getRequest();
+  //   req.user = user;
+  //   return user;
+  // }
 }
